cd4607fb6238a957b14891315841b09724e8f2b4
Md Faisal Raza Khan <irkfaisal@gmail.com>
2022-10-09 16:27:21 +0530

    array insertion

---
+++ Array/Array Insertion/arrayInsertion.js
@@ -0,0 +1,30 @@
+
+let array = [9, 5, 2, 8, 4, 1, 10];
+function insertElement() {
+    let newElem = parseInt(document.getElementById("element").value);
+    let newPosition = parseInt(document.getElementById("position").value);
+    for (let i = array.length - 1; i >= 0; i--) {
+        if (i >= newPosition) {
+            array[i + 1] = array[i]
+            if (i == newPosition) {
+                array[i] = newElem;
+            }
+        }
+    }
+    console.log(array);
+
+}
+
+// adding new element by for loop
+let arr = [9, 5, 2, 8, 4, 1, 10];
+let newElem1 = 0;
+let position1 = 4;
+for (let i = arr.length - 1; i >= 0; i--) {
+    if(i >= position1){
+        arr[i+1] = arr[i]
+        if (i === position1) {
+            arr[i] = newElem1
+        }
+    }
+}
+console.log(arr);
